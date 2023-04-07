38aac7535939b2212236630d7155933e74c1e9f3
porwalarchit <architporwal@gmail.com>
2023-04-07 19:43:32 +0530

    Minor changes

--- src/main/java/com/archit/demo/rest/StudentRestController.java
+++ src/main/java/com/archit/demo/rest/StudentRestController.java
@@ -1,9 +1,10 @@
 package com.archit.demo.rest;
 
 import com.archit.demo.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,16 +12,34 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    // define endpoint for "/students"
+    private List<Student> theStudents;
 
-    @GetMapping("/students")
-    public List<Student> getStudents(){
-        List<Student> theStudents = new ArrayList<>(); 
+    // define @PostConstruct to load the student data ...only once!
+    @PostConstruct
+    public void loadData(){
+        theStudents = new ArrayList<>();
 
         theStudents.add(new Student("Poornima", "Patel"));
         theStudents.add(new Student("Moria", "Rossi"));
         theStudents.add(new Student("Mary", "Smith"));
+    }
 
+    // define endpoint for "/students"
+    @GetMapping("/students")
+    public List<Student> getStudents(){
         return theStudents;
+    }
+
+    // define endpoint or "/students/{studentId}" - return student at index.
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+
+        // check the studentId again list size
+        if((studentId >= theStudents.size() ) || (studentId < 0)){
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+
+        // just index into the list...keep it simple for now.
+        return  theStudents.get(studentId);
     }
 }
